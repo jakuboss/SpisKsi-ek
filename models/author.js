e5f4f427b4d245449bd9cfae3e26419ff4870445
jakuboss <jakub16oss@gmail.com>
2020-05-25 14:55:36 +0200

    pierwsza wersja

---
+++ models/author.js
@@ -0,0 +1,26 @@
+//model dla autorów 
+const mongoose = require('mongoose')
+const Book = require('./book')
+
+const authorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  }
+})
+
+authorSchema.pre('remove', function (next) {
+  Book.find({
+    author: this.id
+  }, (err, books) => {
+    if (err) {
+      next(err)
+    } else if (books.length > 0) {
+      next(new Error('Do tego autora nadal przypisane są książki.'))
+    } else {
+      next()
+    }
+  })
+})
+
+module.exports = mongoose.model('Author', authorSchema)
---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+const Book = require('../models/book')
+
+//Wyświetl stronę główną
+router.get('/', async (req, res) => {
+  let books
+  try {
+    books = await Book.find().sort({
+      createdAt: 'desc'
+    }).limit(10).exec()
+  } catch {
+    books = []
+  }
+  res.render('index', {
+    books: books
+  })
+})
+
+module.exports = router
